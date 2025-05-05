1f69ccd1a64009a1e9eec6de102a3f08c1e77775
Sujal2308 <sujalbhugul08@gmail.com>
2025-05-06 03:56:38 +0530

    feat: add stream.js for handling form submissions via HTTP server
    This commit introduces a new file `stream.js` that sets up an HTTP server to handle GET and POST requests. The server responds to `/form` with an HTML form and processes form submissions at `/submit`, writing the data to a file. This implementation demonstrates streaming responses and handling form data in chunks.

---
+++ Revision/stream.js
@@ -0,0 +1,41 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url.toLowerCase() === "/form" && req.method === "GET") {
+    //! sending response in chunks /stream to the client
+    res.setHeader("Content-Type", "text/html");
+    res.statusCode = 200;
+    res.write("<form action = '/submit' method = 'POST'>");
+    res.write("<label for = 'name'>Name</label>");
+    res.write("<input type = 'text' name = 'username' id = 'name'> </br>");
+    res.write("<label for = 'email'>Email</label>");
+    res.write("<input type = 'email' name = 'email' id = 'email'> </br>");
+    res.write("<input type = 'submit' value = 'Submit'>");
+    res.write("</form>");
+    return res.end();
+  }
+  if (req.url === "/submit" && req.method === "POST") {
+    let data = [];
+    req.on("data", (chunk) => {
+      data.push(chunk);
+    });
+    req.on("end", () => {
+      //   const obj = {};
+      const fullBody = Buffer.concat(data).toString();
+      const formData = new URLSearchParams(fullBody);
+      //   for (const [key, value] of formData) {
+      //     obj[key] = value;
+      //   }
+      const obj = Object.fromEntries(formData);
+      console.log(obj);
+      fs.writeFileSync("formInfo.text", JSON.stringify(obj));
+      res.setHeader("location", "/form");
+    });
+    res.statusCode = 302;
+
+    return res.end();
+  }
+});
+const PORT = 8000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
