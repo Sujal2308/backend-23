213afaa86dd46ff3913116648aa6c38e38181118
Sujal2308 <sujalbhugul08@gmail.com>
2025-05-07 23:54:22 +0530

    feat(middleware): add middleware for /about route and modify existing middleware

--- Revision/Express/server.js
+++ Revision/Express/server.js
@@ -13,14 +13,28 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
+  //! run for all the requests
   console.log("Middleware2 is called: ", req.url);
+  res.send("Hello World!");
+  next();
+});
+
+app.use("/about", function (req, res, next) {
+  //! if the url request is not /about then this middleware will not be called
+  console.log("About page middleware is called");
   next();
 });
 
 //! First GET request
+//! Creating routes
 app.get("/", (req, res) => {
   console.log(req.url, req.method);
   res.send("Hello World!");
+});
+
+app.get("/about", (req, res) => {
+  console.log(req.url, req.method);
+  res.send("<h1>About Page</h1>");
 });
 
 app.listen(port, () => {
